dcdc9f8efbbc69ee4c2d086554b4374636f0a0a0
Crisimple <1240281595@qq.com>
2019-08-19 23:29:59 +0800

    02_controlBroswer control broswer page back or forward

--- doc/02_controlBrowser.py
+++ doc/02_controlBrowser.py
@@ -12,6 +12,7 @@
 
 # 1. 驱动浏览器控制窗口大小
 from selenium import webdriver
+import time
 
 
 base_url = 'http://www.5itest.cn/register'
@@ -22,3 +23,28 @@ browser.get(base_url)
 # browser.set_window_size(400, 800)
 browser.maximize_window()
 browser.quit()
+
+
+# 2. 控制浏览器的前进、后退
+browser_links = webdriver.Chrome('../tools/chromedriver.exe')
+first_url = 'https://www.baidu.com/'
+second_url = 'https://news.baidu.com/'
+
+print("访问第一个链接：%s" % first_url)
+browser_links.get(first_url)
+
+time.sleep(1)
+print("访问第二个链接：%s" % second_url)
+browser_links.get(second_url)
+
+time.sleep(1)
+print("回退到第一个链接：%s" % first_url)
+browser_links.back()
+
+time.sleep(1)
+print("前进到第二个链接：%s", second_url)
+browser_links.forward()
+
+time.sleep(1)
+browser_links.quit()
+
