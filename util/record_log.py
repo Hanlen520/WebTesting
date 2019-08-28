e6b6dd71e86f8c7d4b591628ea7a64816acfd184
crisimple <crisimple@foxmail.com>
2019-08-28 19:58:00 +0800

    record_log write log to log_file

--- util/record_log.py
+++ util/record_log.py
@@ -11,6 +11,8 @@
 """
 
 import logging
+import os
+from datetime import datetime
 
 
 class RecordLog(object):
@@ -31,6 +33,33 @@ class RecordLog(object):
         # 移除
         self.logger.removeHandler(console)
 
+        # 2.将log信息输出到log文件中
+        # 2.1 先定位看将log文件输出到哪里去
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        print(current_dir)  # D:\MySpace\Python\WebTesting\util
+        log_dir = os.path.join('../logs')
+        # 日志名称构建
+        log_file_name = datetime.now().strftime("%Y-%m-%d") + '.log'
+        log_file_path = log_dir + '/' + log_file_name
+        print(log_file_path)
+
+        # 2.2 好的，将日志写进log文件中
+        self.file_handle = logging.FileHandler(log_file_path, 'a', encoding='utf-8')
+        formatter = logging.Formatter(
+            '%(asctime)s %(filename)s %(funcName)s %(levelno)s: [%(levelname)s] ---> %(message)s')
+        self.file_handle.setFormatter(formatter)
+        self.logger.addHandler(self.file_handle)
+
+    def get_log(self):
+        return self.logger
+
+    def close_handle(self):
+        self.logger.removeHandler(self.file_handle)
+        self.file_handle.close()
+
 
 if __name__ == "__main__":
     rl = RecordLog()
+    log_info = rl.get_log()
+    log_info.debug('输出到文件中去')
+    rl.close_handle()
